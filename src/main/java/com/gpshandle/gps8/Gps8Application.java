3a3defac7c15029dcddf5bd94532dbc1d50014db
duvu <hoaivubk@gmail.com>
2023-08-17 20:42:43 +0700

    Init commit

---
+++ src/main/java/com/gpshandle/gps8/Gps8Application.java
@@ -0,0 +1,13 @@
+package com.gpshandle.gps8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gps8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gps8Application.class, args);
+    }
+
+}
